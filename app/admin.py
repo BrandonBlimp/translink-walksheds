5fa353fc658f964a3088f61e5e8d944b0320adaa
BrandonBlimp <brandonloong.lim@gmail.com>
2019-04-02 18:42:09 -0700

    added Shape, Trip, and StopTime models

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from .models import Stop, Route, Shape, Trip, StopTimes
+
 # Register your models here.
+
+admin.site.register(Stop)
+admin.site.register(Route)
+admin.site.register(Shape)
+admin.site.register(Trip)
+admin.site.register(StopTimes)
--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
-# Create your models here.
+# See the Google GTFS documentation for details
+# https://developers.google.com/transit/gtfs/reference/
 
 class Stop(models.Model):
     LOCATION_TYPE_STOP = 1
@@ -13,12 +14,15 @@ class Stop(models.Model):
     )
 
     stop_id = models.IntegerField(primary_key=True)
-    stop_code = models.IntegerField(blank=True)
+    stop_code = models.CharField(max_length=10, blank=True, default="")
     stop_name = models.CharField(max_length=200)
     stop_lat = models.DecimalField(max_digits=9, decimal_places=6)
     stop_lon = models.DecimalField(max_digits=9, decimal_places=6)
     zone_id = models.CharField(max_length=50, blank=True)
-    location_type = models.IntegerField(choices=LOCATION_TYPE_CHOICES, blank=True)
+    location_type = models.IntegerField(choices=LOCATION_TYPE_CHOICES, blank=True, null=True)
+
+    def __str__(self):
+        return ",".join([str(self.stop_id), self.stop_name])
 
 class Route(models.Model):
     ROUTE_TYPE_LRT = 0
@@ -47,3 +51,82 @@ class Route(models.Model):
     route_type = models.IntegerField(choices=ROUTE_TYPE_CHOICES)
     route_color = models.CharField(max_length=6, default="FFFFFF", blank=True)
     route_text_color = models.CharField(max_length=6, default="000000", blank=True)
+
+
+class Shape(models.Model):
+    shape_id = models.CharField(max_length=20)
+    shape_pt_lat = models.DecimalField(max_digits=9, decimal_places=6)
+    shape_pt_lon = models.DecimalField(max_digits=9, decimal_places=6)
+    shape_pt_sequence = models.PositiveIntegerField()
+    shape_dist_traveled = models.FloatField()
+
+
+class Trip(models.Model):
+    DIRECTION_ID_CHOICES = (
+        (0,"direction 0"),
+        (1,"direction 1")
+    )
+
+    WHEELCHAIR_ACCESSIBLE_UNKNOWN = 0
+    WHEELCHAIR_ACCESSIBLE_YES = 1
+    WHEELCHAIR_ACCESSIBLE_NO = 2
+    WHEELCHAIR_ACCESSIBLE_CHOICES = (
+        (WHEELCHAIR_ACCESSIBLE_UNKNOWN, "No accessibility info"),
+        (WHEELCHAIR_ACCESSIBLE_YES, "Wheelchair-accessible"),
+        (WHEELCHAIR_ACCESSIBLE_NO, "Not wheelchair-accessible")
+    )
+
+    BIKES_ALLOWED_UNKNOWN = 0
+    BIKES_ALLOWED_YES = 1
+    BIKES_ALLOWED_NO = 2
+    BIKES_ALLOWED_CHOICES = (
+        (BIKES_ALLOWED_UNKNOWN, "No bike info"),
+        (BIKES_ALLOWED_YES, "Bikes allowed"),
+        (BIKES_ALLOWED_NO, "Bikes not allowed")
+    )
+
+    trip_id = models.CharField(max_length=20, primary_key=True)
+    route_id = models.ForeignKey(Route, on_delete=models.CASCADE)
+    service_id = models.CharField(max_length=10)
+    trip_headsign = models.CharField(max_length=100, blank=True)
+    direction_id = models.IntegerField(choices=DIRECTION_ID_CHOICES)
+    block_id = models.CharField(max_length=100, blank=True)
+    shape_id = models.ForeignKey(Shape, on_delete=models.SET_NULL, null=True)
+    wheelchair_accessible = models.IntegerField(choices=WHEELCHAIR_ACCESSIBLE_CHOICES, default=0, blank=True,)
+    direction_id = models.IntegerField(choices=DIRECTION_ID_CHOICES)
+    bikes_allowed = models.IntegerField(choices=BIKES_ALLOWED_CHOICES, default=0, blank=True)
+
+
+class StopTimes(models.Model):
+    PICKUP_TYPE_REGULAR = 0
+    PICKUP_TYPE_NONE = 1
+    PICUKP_TYPE_PHONE_REQUEST = 2
+    PICKUP_TYPE_DRIVER_COORD = 3
+    PICKUP_TYPE_CHOICES = (
+        (PICKUP_TYPE_REGULAR, "Regularly scheduled pickup"),
+        (PICKUP_TYPE_NONE, "No pickup available"),
+        (PICUKP_TYPE_PHONE_REQUEST, "Phone agency to arrange pickup"),
+        (PICKUP_TYPE_DRIVER_COORD, "Coordinate with driver to arrange pickup")
+    )
+
+    DROPOFF_TYPE_REGULAR = 0
+    DROPOFF_TYPE_NONE = 1
+    PICUKP_TYPE_PHONE_REQUEST = 2
+    DROPOFF_TYPE_DRIVER_COORD = 3
+    DROPOFF_TYPE_CHOICES = (
+        (DROPOFF_TYPE_REGULAR, "Regularly scheduled drop off"),
+        (DROPOFF_TYPE_NONE, "No drop off available"),
+        (PICUKP_TYPE_PHONE_REQUEST, "Phone agency to arrange drop off"),
+        (DROPOFF_TYPE_DRIVER_COORD, "Coordinate with driver to arrange drop off")
+    )
+
+    trip_id = models.ForeignKey(Trip, on_delete=models.CASCADE)
+    arrival_time = models.DurationField()
+    departure_time = models.DurationField()
+    stop_id = models.ForeignKey(Stop, on_delete=models.CASCADE)
+    stop_sequence = models.PositiveIntegerField()
+    stop_headsign = models.CharField(max_length=100, blank=True)
+    pickup_type = models.IntegerField(choices=PICKUP_TYPE_CHOICES, default=PICKUP_TYPE_REGULAR, blank=True)
+    drop_off_type = models.IntegerField(choices=DROPOFF_TYPE_CHOICES, default=PICKUP_TYPE_REGULAR, blank=True)
+    shape_dist_traveled = models.FloatField(blank=True)
+
