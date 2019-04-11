adf12ada1b0a8e7cb2cb4c13c5dc121ff61456e2
BrandonBlimp <brandonloong.lim@gmail.com>
2019-04-10 17:17:00 -0700

    added basic html template and css

--- app/views.py
+++ app/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from .models import Route
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world")
+    random_list = Route.objects.all()
+    template = loader.get_template('app/index.html')
+    context = {
+        'random_list': random_list
+    }
+    return HttpResponse(template.render(context, request))
