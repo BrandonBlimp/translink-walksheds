bff003e45508f2ee2f04d44995d1c59acfc9cd9d
BrandonBlimp <brandonloong.lim@gmail.com>
2019-05-14 15:10:12 -0700

    experimenting with ajax

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,7 +1,28 @@
 $(document).ready(function () {
+    $('#routeSelector .dropdown-item').click(changeRoute);
+});
 
-    $('#routeSelector .dropdown-item').click( function() {
-        $('#dropdownMenuButton').text($(this).text());
+function changeRoute () {
+    $('#dropdownMenuButton').text($(this).text());
+
+    var url = "/routes/"
+    var routeId = 
+
+    $.ajax({
+        url: url,
+        method: "GET",
+        data: {
+            "route_id": 6771,
+        },
+        dataType: "json",
+        success: function (response) {
+            // $('#graph-overlay').find('.graph-content').html(response);
+            // $('#graph-overlay').fadeIn(300);
+            // hideLoadingOverlay();
+            alert("it worked kinda");
+        },
+        error: function () {
+            alert("oops");
+        }
     });
-
-})
+}
