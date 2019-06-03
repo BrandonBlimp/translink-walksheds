17a8a618cbb79834b0a062c30fcbb95a7658a60d
BrandonBlimp <brandonloong.lim@gmail.com>
2019-06-03 13:42:38 -0700

    added custom method onto TripViewSet to view distinct headsigns

--- api/views.py
+++ api/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 
+from app.models import Route, Shape, Trip
 from rest_framework import viewsets
+from rest_framework.decorators import list_route
+from rest_framework.response import Response
 
-from app.models import Route, Trip, Shape
-from .serializers import RouteSerializer, TripSerializer, ShapeSerializer
+from .serializers import RouteSerializer, ShapeSerializer, TripSerializer
 
 
 class RouteViewSet(viewsets.ReadOnlyModelViewSet):
@@ -22,6 +24,15 @@ class TripViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Trip.objects.all()
     serializer_class = TripSerializer
     filterset_fields = ('route__route_id',)
+
+    # this decorator allows routing to the distinct_headsigns method
+    # access this method with the URL /trips/distinct_headsigns/
+    # NOTE: the .distinct() method ONLY WORKS IF DATABASE IS POSTGRES
+    @list_route()
+    def distinct_headsigns(self, request):
+        query_set = self.filter_queryset(self.get_queryset().distinct('trip_headsign'))
+        serializer = self.get_serializer(query_set, many=True)
+        return Response(serializer.data)
 
 class ShapeViewSet(viewsets.ReadOnlyModelViewSet):
     """
