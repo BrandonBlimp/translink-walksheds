6a7898ce24957d7e3ba0a1ebc1f6b455120ff474
BrandonBlimp <brandonloong.lim@gmail.com>
2019-06-03 14:52:38 -0700

    - changed ajax call to return trip destinations based on selected route - added dropdown that is populated by trip destination headsigns on ajax success

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -5,24 +5,32 @@ $(document).ready(function () {
 function changeRoute () {
     $('#routeSelectorButton').text($(this).text());
 
-    var url = "/routes/"
+    var url = "/trips/distinct_headsigns"
     var routeId = $(this).data("route-id");
 
     $.ajax({
         url: url,
         method: "GET",
         data: {
-            "route_id": routeId,
+            "route__route_id": routeId,
         },
         dataType: "json",
+        context: this,
         success: function (response) {
-            // $('#graph-overlay').find('.graph-content').html(response);
-            // $('#graph-overlay').fadeIn(300);
-            // hideLoadingOverlay();
-            alert(routeId);
+            loadDestinations(response);
         },
         error: function () {
             alert("oops");
         }
     });
+}
+
+function loadDestinations (response) {
+    $('#destSelectorButton').prop('disabled', false);
+
+    var len = response.length;
+    for (var i=0; i<len; i++) {
+        var tripHeadsign = response[i]["trip_headsign"]
+        $('#destSelector').append("<a class='dropdown-item' href='#'>" + tripHeadsign + "</a>");
+    }
 }
