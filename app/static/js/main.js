0dbed8edbe356ec6efb9d59b79937e5038751bea
BrandonBlimp <brandonloong.lim@gmail.com>
2019-06-03 16:50:27 -0700

    selecting a route + destination now displays the path on the map in a red line - changed viewset so shapes are returned in order of sequence number so shapes are rendered correctly

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,8 +1,9 @@
 $(document).ready(function () {
-    $('#routeSelector .dropdown-item').click(changeRoute);
+    $('#routeSelector .dropdown-item').click(selectRoute);
 });
 
-function changeRoute () {
+function selectRoute () {
+    // change text displayed on dropdown button
     $('#routeSelectorButton').text($(this).text());
 
     var url = "/trips/distinct_headsigns"
@@ -31,7 +32,57 @@ function loadDestinations (response) {
 
     var len = response.length;
     for (var i=0; i<len; i++) {
-        var tripHeadsign = response[i]["trip_headsign"]
-        $('#destSelector').append("<a class='dropdown-item' href='#'>" + tripHeadsign + "</a>");
+        var tripHeadsign = response[i]["trip_headsign"];
+        var shapeID = response[i]["shape_id"];
+        $('#destSelector').append("<a class='dropdown-item' data-shape-id='" + shapeID + "' href='#'>" + tripHeadsign + "</a>");
     }
+
+    // add onclick method to dropdown items
+    $('#destSelector .dropdown-item').click(selectDestination);
+}
+
+function selectDestination () {
+    // change text displayed on dropdown button
+    $('#destSelectorButton').text($(this).text());
+
+    var url = "/shapes/"
+    var shapeID = $(this).data("shape-id");
+
+    $.ajax({
+        url: url,
+        method: "GET",
+        data: {
+            "shape_id": shapeID,
+        },
+        dataType: "json",
+        context: this,
+        success: function (response) {
+            drawShapes(response);
+        },
+        error: function () {
+            alert("oops");
+        }
+    });
+}
+
+function drawShapes (shapes) {
+    var coords = shapes.map(function (x) {
+        return {
+            lat: parseFloat(x["shape_pt_lat"]),
+            lng: parseFloat(x["shape_pt_lon"])};
+    });
+    
+    var routeDestinationLine = new google.maps.Polyline({
+        path: coords,
+        geodesic: true,
+        strokeColor: '#FF0000',
+        strokeOpacity: 1.0,
+        strokeWeight: 2
+    });
+    routeDestinationLine.setMap(map);
+    // alert("halp");
+}
+
+function clearMap () {
+
 }
